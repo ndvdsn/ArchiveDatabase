fd0caf7438d0d63879c82ff108c71ae4827f88a9
ndvdsn <neil.j.davidson@gmail.com>
2019-07-26 16:23:22 +0100

    refactors artist component

--- frontend/archive_database/src/components/artists/Artist.js
+++ frontend/archive_database/src/components/artists/Artist.js
@@ -7,71 +7,85 @@ const Artist = (props) => {
     return null;
   }
 
-  const dateBasic = props.artist.singleEvents.map((singleEvent)=>{
-    return singleEvent.date
-  })
-
-  const time = props.artist.singleEvents.map((singleEvent)=>{
-    return singleEvent.time
-  })
-  // const slicedDate = dateBasic.slice(0,10)
-  //
-  // const formattedDate = moment(slicedDate).format("dddd Do MMM YYYY");
-  // const time = moment(slicedDate).format('hh:mm z','Europe/London');
-
   const id = props.artist.id
+  const artistName = props.artist.name;
+  const artistCountry = props.artist.country;
 
-  const groups = props.artist.groups.map((group, index)=>{
-    return <ul key={index}>{group.name}</ul>
-  })
 
-  const singleEvent = props.artist.singleEvents.map((singleEvent, index) => {
-    return <ul key={index}>{singleEvent.title} {singleEvent.location} {singleEvent.venue}</ul>
+  const artist_singleEvents = props.artist.singleEvents.map((singleEvent, index) => {
+      return(
+        <li key= {singleEvent.id} className = "artist-singleEvent">
+        <div className="single-artist-singleEvent">
+        {singleEvent.id}:
+        </div><div>{singleEvent.title} {singleEvent.location}{singleEvent.venue}{singleEvent.date}{singleEvent.time}</div>
+        </li>
+      )
+    })
 
-  })
-
-  const location = props.artist.singleEvents.map((singleEvent, index) => {
-    return <ul key={index}>{singleEvent.location}</ul>
-  })
-
-  const venue = props.artist.singleEvents.map((singleEvent, index) => {
-    return <ul key={index}>{singleEvent.venue}</ul>
+  const artist_groups = props.artist.groups.map((group, index) => {
+    return(
+      <li key={group.id} className = "artist-group">
+      <div className="single-artist-group">
+      {group.id}
+      </div><div>{group.name}</div>
+      </li>
+    )
   })
 
   return(
-    <div className = "single-artist">
-      <div className="details">
+    <div className = "component">
       <ul>
         <div>
-        <li><a href={'/artists/'+id}>{props.artist.name}</a></li>
+        <li><a href={'/artists/'+id}>{artistName}</a></li>
         </div>
         <div>
-        <li>Country: {props.artist.country}</li>
+        <li>Country: {artistCountry}</li>
         </div>
         <div>
-        <li>Member of groups: {groups}</li>
         </div>
         <h4>Participated in events:</h4>
         <div className="artist-singleEvent-container">
           <div>
-          <li>Title: {singleEvent}</li>
+          <ul>Title: {artist_singleEvents}</ul>
           </div>
           <div>
-          <li>Date: {dateBasic}</li>
-          </div>
-          <div>
-          <li>Time: {time}</li>
-          </div>
-          <div>
-          <li>Location: {location}</li>
-          </div>
-          <div>
-          <li>Venue: {venue}</li>
+          <ul>Groups: {artist_groups}</ul>
           </div>
         </div>
       </ul>
-      </div>
     </div>
   )
 }
+
 export default Artist;
+
+
+// const singleEvent = props.artist.singleEvents.map((singleEvent, index) => {
+//   return <ul key={index}>{singleEvent.title} {singleEvent.location} {singleEvent.venue}</ul>
+// })
+//
+// const location = props.artist.singleEvents.map((singleEvent, index) => {
+//   return <ul key={index}>{singleEvent.location}</ul>
+// })
+//
+// const venue = props.artist.singleEvents.map((singleEvent, index) => {
+//   return <ul key={index}>{singleEvent.venue}</ul>
+// })
+//
+// const dateBasic = props.artist.singleEvents.map((singleEvent)=>{
+//   return singleEvent.date
+// })
+//
+// const time = props.artist.singleEvents.map((singleEvent)=>{
+//   return singleEvent.time
+// })
+// const slicedDate = dateBasic.slice(0,10)
+//
+// const formattedDate = moment(slicedDate).format("dddd Do MMM YYYY");
+// const time = moment(slicedDate).format('hh:mm z','Europe/London');
+
+
+
+// const groups = props.artist.groups.map((group, index)=>{
+//   return <ul key={index}>{group.name}</ul>
+// })
